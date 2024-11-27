e4cd3245aaab90282b40dfbd871a4a2a5458a05b
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-11-27 23:02:20 +0300

    create Pagination page

---
+++ src/app/_lib/Pagination.tsx
@@ -0,0 +1,33 @@
+// "use client"
+import { Dispatch, SetStateAction } from "react";
+import { Post } from "../blog/page"
+
+const Pagination = ({posts, ipp, setCurrentPageData}: {
+    posts:Post[],
+    ipp: number,
+    setCurrentPageData: Dispatch<SetStateAction<Post[]>>
+}) => {
+
+  return (
+    <ul className="flex gap-3 self-center text-black p-1">
+    {/* map function goes here */}
+        <li>{"<"}</li>
+        {Array(Math.ceil(posts.length / ipp)).fill("").map((_item,index) => {
+            return <li onClick={()=> {
+                                const page = index + 1;
+                                const start  = ((page - 1 ) * ipp);
+                                const end = page * ipp;
+                                const curatedPosts = posts.slice(start, end);
+                                setCurrentPageData(curatedPosts)
+
+            }} style={{background: ""}} key={index} className="underline cursor-pointer">{index+1}</li>; {/*this fetches with the amount set on ipp*/}
+        })}
+        {/* <li className="underline cursor-pointer">1</li> this fetches with the amount set on ipp */}
+        {/* <li className="underline cursor-pointer">2</li> same as above */}
+        {/* <li className="underline cursor-pointer">3</li> same as above */}
+        <li>{">"}</li>
+</ul>
+  )
+}
+
+export default Pagination
