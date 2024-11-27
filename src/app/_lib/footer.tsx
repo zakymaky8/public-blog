18bce2ed9b1aeb2455a353f6b923e28adeb8f441
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-11-27 22:57:59 +0300

    create footer

---
+++ src/app/_lib/footer.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+const Footer = () => {
+  return (
+    <footer className="bg-black text-gray-500 h-10 flex justify-center items-center"> {/*work on this crap*/}
+        <Link href="/" >zeki</Link>
+    </footer>
+  )
+}
+
+export default Footer
