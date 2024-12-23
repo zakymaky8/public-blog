ae598d160e435dcc745fe54d20a5d49f7631925d
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-12-23 16:21:00 +0300

    register route

---
+++ src/app/register/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+import SignUpForm from "../_lib/SignUpForm"
+
+export const metadata = {
+  title: "Register"
+}
+
+const RegistrationPage = () => {
+  return (
+    <div className="w-full flex items-center flex-col">
+        <br />
+        <h1  className="text-gray-900">Sign Up</h1>
+        <SignUpForm />
+        <span className="text-gray-900">Already Have One? <Link href="/login" className="underline">Log in</Link></span>
+    </div>
+  )
+}
+
+export default RegistrationPage
