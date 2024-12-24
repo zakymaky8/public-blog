7324af4778348d4f1b1170e3d25d9c1d37e445a2
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-12-24 14:33:13 +0300

    add intercepting register route

---
+++ src/app/(.)register/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import SignUpForm from "../_lib/SignUpForm"
+
+export const metadata = {
+  title: "Register"
+}
+
+const RegistrationPage = () => {
+  return (
+    <div className=" w-full flex justify-center z-20 h-full flex-auto mt-5 p-5">
+      <div className=" flex items-center bg-slate-900 bg-opacity-80 flex-col p-5">
+        <br />
+        <h1  className="text-gray-900">Sign Up</h1>
+        <SignUpForm />
+        <span className="text-gray-900">Already Have One? <Link href="/login" className="no-underline text-white opacity-70 hover:opacity-100">Log in</Link></span>
+      </div>
+    </div>
+  )
+}
+
+export default RegistrationPage
