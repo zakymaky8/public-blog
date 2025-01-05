e85e0a8fd6e614da2a4128d5127e211fe310c984
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2025-01-05 22:47:43 +0300

    edit styling in comment section

--- src/app/_lib/CommentsCard.tsx
+++ src/app/_lib/CommentsCard.tsx
@@ -50,28 +50,30 @@ const CommentsCard = async ({postId}: TProps) => {
     }
 
   return (
-    <div>
+    <div className="bg-slate-200 p-3 rounded-xl">
       <h2 className="font-bold text-xl mb-5">Comments: {comments.length}</h2>
-        {comments.map((comment) => {
+        {comments.length ? 
+          comments.map((comment) => {
             const commentIsLiked = comment.likes.includes(currentUser.users_id) ? true : false
             const commentAuthor = findAuthor(comment.user_id);
             const thisCommentReply = replies.filter(reply => reply.comment_id === comment.comments_id) || null;
             return (
-              <div key={comment.comments_id} className="flex flex-col gap-3">
-              <SingleComment
-                    comment={comment}
-                    commentAuthor={commentAuthor}
-                    commentIsLiked={commentIsLiked}
-                    currentUser={currentUser}
-                    authorname = {findAuthor(comment.user_id)}
-                    postId= {postId}
-                    />
+              <div key={comment.comments_id} className="flex flex-col gap-3 font-serif">
+                <SingleComment
+                      comment={comment}
+                      commentAuthor={commentAuthor}
+                      commentIsLiked={commentIsLiked}
+                      currentUser={currentUser}
+                      authorname = {findAuthor(comment.user_id)}
+                      postId= {postId}
+                />
 
-                  <Replies currentUser={currentUser} postId={postId} comment={comment} replyActorPairs={replyActorPairs} commentReply={thisCommentReply} />
-                  <hr className="border-[1px] border-black opacity-20"/>
+                <Replies currentUser={currentUser} postId={postId} comment={comment} replyActorPairs={replyActorPairs} commentReply={thisCommentReply} />
+                <hr className="border-[1px] border-black opacity-20"/>
               </div>
             )
-        })}
+        })
+      : <p className="text-center mt-10 opacity-70 text-[14px]">No comments yet!</p>}
     </div>
   )
 }
