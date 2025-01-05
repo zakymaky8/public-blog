bc1a9d39976078ba7c79002c6871b612f4607ad2
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2025-01-05 22:46:34 +0300

    modiify blog card

--- src/app/_lib/BlogCard.tsx
+++ src/app/_lib/BlogCard.tsx
@@ -1,20 +1,41 @@
 import Link from "next/link"
+import { Post } from "../blog/page";
+import { cookies } from "next/headers";
 
 interface BlogInfo {
-    id: number,
+    id: string,
     title: string,
     content: string,
-    commentAmt: number
+    post: Post,
+    readTime: number,
+    excerpt: string
 }
 
-const BlogCard = ({id, title, content, commentAmt}: BlogInfo) => {
+const BlogCard = async ({id, title, excerpt, post, readTime}: BlogInfo) => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value
+  const res = await fetch(`http://localhost:3456/posts/${id}/comments`, {
+    headers: {
+        "authorization": `Bearer ${token}`,
+        "content-type": "application/json"
+      }
+  });
+  const { data } = await res.json();
+  const [comments] = data;
+
+
   return (
-    <div className="w-64 h-56 bg-gray-900 rounded-xl text-stone-300 p-3 flex flex-col justify-between">
-        <h2 className="text-xl">{title}</h2>
-        <p className="text-xs italic">{content}</p>
-        <div className="flex justify-between">
-            <p>Comments: {Number(commentAmt)}</p>
-            <Link href={`/blog/${id}`} className="underline">Read more</Link>
+    <div className="max-w-[600px] gap-2 min-h-56 bg-[#c8daff] text-[#111827] rounded-md border-t-[13px] border-b-[13px] border-[#111827]  p-3 pl-6 pr-6 flex flex-col justify-between"
+      style={{boxShadow: "inset 0 0 20px 0 #111827"}}>
+        <h2 className="text-2xl font-bold">{title.toUpperCase()[0] + title.slice(1, )}</h2>
+        <p className="text-[13px] italic">{excerpt} ...</p>
+        <div className="flex justify-between items-center flex-wrap gap-2">
+            <div className="flex gap-2 text-[14px] text-green-700">
+              <p>{readTime} min Read,</p>
+              <p>Comments: {comments.length}</p>
+              <p>Likes: {post.likes.length}</p>
+            </div>
+            <Link href={`/blog/${id}`} className="text-[#f7f8f9] bg-[#111827] no-underline  text-opacity-50 p-[6px] rounded-md hover:text-opacity-100">Read more</Link>
         </div>
     </div>
   )
