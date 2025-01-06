8dd966709a4efe5592ed1a0b0c498c034bd08822
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2025-01-06 23:40:06 +0300

    modify footer

--- src/app/_lib/footer.tsx
+++ src/app/_lib/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Footer = () => {
   return (
-    <ul className="flex gap-5 bg-[#181b6b] p-5 -mb-2 text-[11px] self-center w-full justify-center">
+    <ul className="flex gap-5 bg-[#181b6b] flex-wrap p-5 -mb-2 text-[11px] self-center w-full justify-center">
         <Link href="https://zachfolio.vercel.app/contact" className="text-white hover:text-yellow-400">Contact me</Link >
         <Link href="/somewhere" className="text-white hover:text-yellow-400">Ask Help</Link >
         <Link href="/somewhere" className="text-white hover:text-yellow-400">Work With me</Link >
