b6bd24c47a8b92774c6e8b48b214eaa4c26175e0
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2025-01-06 23:41:08 +0300

    modify font

--- src/app/_lib/BlogCard.tsx
+++ src/app/_lib/BlogCard.tsx
@@ -27,7 +27,7 @@ const BlogCard = async ({id, title, excerpt, post, readTime}: BlogInfo) => {
   return (
     <div className="max-w-[600px] gap-2 min-h-56 bg-[#c8daff] text-[#111827] rounded-md border-t-[13px] border-b-[13px] border-[#111827]  p-3 pl-6 pr-6 flex flex-col justify-between"
       style={{boxShadow: "inset 0 0 20px 0 #111827"}}>
-        <h2 className="text-2xl font-bold">{title.toUpperCase()[0] + title.slice(1, )}</h2>
+        <h2 className="text-2xl font-[1000] font-serif">{title.toUpperCase()[0] + title.slice(1, )}</h2>
         <p className="text-[13px] italic">{excerpt} ...</p>
         <div className="flex justify-between items-center flex-wrap gap-2">
             <div className="flex gap-2 text-[14px] text-green-700">
