38ee0db356f553b1f62a5a61e0880f620e03449c
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2025-01-19 23:44:35 +0300

    make footer responsive

--- src/app/_lib/footer.tsx
+++ src/app/_lib/footer.tsx
@@ -2,14 +2,37 @@ import Link from "next/link"
 
 const Footer = () => {
   return (
-    <ul className="flex gap-5 bg-[#181b6b] flex-wrap p-5 -mb-2 text-[11px] self-center w-full justify-center">
-        <Link href="https://zachfolio.vercel.app/contact" className="text-white hover:text-yellow-400">Contact me</Link >
-        <Link href="/somewhere" className="text-white hover:text-yellow-400">Ask Help</Link >
-        <Link href="/somewhere" className="text-white hover:text-yellow-400">Work With me</Link >
-        <Link href="https://zachfolio.vercel.app/about" className="text-white hover:text-yellow-400">About Me</Link >
-        <Link href="/somewhere" className="text-white hover:text-yellow-400">About This Blog</Link >
-        <Link href="/somewhere" className="text-white hover:text-yellow-400">Contribute</Link >
-    </ul>
+    <div className="bg-[#181b6b] w-full text-[11px]">
+      <ul className="flex gap-5 flex-wrap p-8 self-center justify-around footer mb-10">
+
+          <div className=" font-medium text-white flex gap-2 items-center">
+            <strong className="text-lg"> Tip Logger</strong>
+            <span className="text-[13px] mt-1">grab something you might not know</span>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Link href="https://zachfolio.vercel.app/contact" className="text-white hover:text-yellow-400">Contact me</Link >
+            <Link href="/somewhere" className="text-white hover:text-yellow-400">Work With me</Link >
+          </div>
+
+          <div className="flex flex-col gap-2">
+          <Link href="/blog" className="text-white hover:text-yellow-400">Blogs</Link >
+            <Link href="https://zachfolio.vercel.app/about" className="text-white hover:text-yellow-400">About Me</Link >
+            <Link href="/somewhere" className="text-white hover:text-yellow-400">About This Blog</Link >
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Link href="" className="text-white hover:text-yellow-400">Telegram</Link>
+            <Link href="" className="text-white hover:text-yellow-400">Youtube</Link>
+            <Link href="" className="text-white hover:text-yellow-400">LinkedIn</Link>
+          </div>
+          <div className="flex flex-col gap-2">
+            <Link href="/somewhere" className="text-white hover:text-yellow-400">Ask Help</Link >
+            <Link href="/somewhere" className="text-white hover:text-yellow-400">Contribute</Link >
+          </div>
+      </ul>
+      <p className="text-center text-white">By Zechariah @All rights reserved!</p>
+    </div>
   )
 }
 
